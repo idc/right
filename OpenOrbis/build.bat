51dd0ef8bec598b31aa6a21e66423bdd53e98bed
idc <idc@users.noreply.github.com>
2020-05-13 22:03:32 -0500

    Overhaul build script to be more like a makefile.

--- OpenOrbis/build.bat
+++ OpenOrbis/build.bat
@@ -1,52 +1,71 @@
 SETLOCAL EnableDelayedExpansion
 
-REM Libraries to link in
-SET libraries=-lc -lkernel
+SET TOOLCHAIN=%OO_PS4_TOOLCHAIN%
 
-REM Read the script arguments into local vars
-SET intdir=%1
-SET targetname=%~2
-SET outputPath=%~3
+SET LIBS=-lc -lkernel
 
-SET outputElf=%intdir%%targetname%.elf
-SET outputOelf=%intdir%%targetname%.oelf
-SET outputPrx=%intdir%%targetname%.prx
-SET outputStub=%intdir%%targetname%_stub.so
+REM Compiler options. You likely won't need to touch these.
+SET CC=clang
+SET CXX=clang++
+SET LD=ld.lld
 
-REM Compile object files for all the source files
+SET TARGET=%~2
+
+SET ODIR=%1
+SET SDIR=%~3
+
+REM Trim trailing \'s
+IF %TOOLCHAIN:~-1%==\ SET TOOLCHAIN=%TOOLCHAIN:~0,-1%
+IF      %ODIR:~-1%==\ SET ODIR=%ODIR:~0,-1%
+IF      %SDIR:~-1%==\ SET SDIR=%SDIR:~0,-1%
+
+SET IDIRS="-I%TOOLCHAIN%\include"
+SET LDIRS="-L%TOOLCHAIN%\lib"
+
+SET CFLAGS=-cc1 -triple x86_64-scei-ps4-elf -munwind-tables %IDIRS% -emit-obj
+SET CXXFLAGS=%CFLAGS%
+SET LFLAGS=-m elf_x86_64 -pie --version-script="%SDIR%\%TARGET%.version" --script "%TOOLCHAIN%\link.x" --eh-frame-hdr %LDIRS% %LIBS% --verbose "%TOOLCHAIN%\lib\crtlib.o"
+
+SET STUBCFLAGS=-target x86_64-pc-linux-gnu -ffreestanding -nostdlib -fno-builtin -fPIC -c %IDIRS%
+SET STUBCXXFLAGS=%STUBCFLAGS%
+SET STUBLFLAGS=-target x86_64-pc-linux-gnu -shared -fuse-ld=lld -ffreestanding -nostdlib -fno-builtin %LDIRS% %LIBS%
+
+REM Make!
+
 FOR %%f in (..\*.c) DO (
     ECHO Compiling %%~f...
-    clang   -cc1 -triple x86_64-scei-ps4-elf -munwind-tables -I"%OO_PS4_TOOLCHAIN%\include" -emit-obj -o %intdir%\%%~nf.o "%%~f"
+    %CC% %CFLAGS% -o %ODIR%\%%~nf.o "%%~f"
 )
 FOR %%f in (..\*.cpp) DO (
     ECHO Compiling %%~f...
-    clang++ -cc1 -triple x86_64-scei-ps4-elf -munwind-tables -I"%OO_PS4_TOOLCHAIN%\include" -emit-obj -o %intdir%\%%~nf.o "%%~f"
+    %CXX% %CXXFLAGS% -o %ODIR%\%%~nf.o "%%~f"
 )
 
 REM Get a list of object files for linking
-SET obj_files=
-FOR %%f IN (%intdir%\\*.o) DO SET obj_files=!obj_files! .\%%f
+SET OBJS=
+FOR %%f IN (%ODIR%\*.o) DO SET OBJS=!OBJS! %%f
 
 REM Link the input ELF
 ECHO Linking...
-ld.lld -m elf_x86_64 -pie --version-script="%targetname%.version" --script "%OO_PS4_TOOLCHAIN%\link.x" --eh-frame-hdr -o "%outputElf%" "-L%OO_PS4_TOOLCHAIN%\lib" -lc -lkernel --verbose "%OO_PS4_TOOLCHAIN%\lib\crtlib.o" %obj_files%
+%LD% %OBJS% -o "%ODIR%\%TARGET%.elf" %LFLAGS%
 
 REM Create stub shared libraries
 FOR %%f in (..\*.c) DO (
-    clang -target x86_64-pc-linux-gnu -ffreestanding -nostdlib -fno-builtin -fPIC -c -I"%OO_PS4_TOOLCHAIN%\include" -o %intdir%\%%~nf.o.stub "%%~f"
+    %CC% %STUBCFLAGS% -o %ODIR%\%%~nf.o.stub "%%~f"
 )
 FOR %%f in (..\*.cpp) DO (
-    clang++ -target x86_64-pc-linux-gnu -ffreestanding -nostdlib -fno-builtin -fPIC -c -I"%OO_PS4_TOOLCHAIN%\include" -o %intdir%\%%~nf.o.stub "%%~f"
+    %CXX% %STUBCXXFLAGS% -o %ODIR%\%%~nf.o.stub "%%~f"
 )
 
-SET stub_obj_files=
-FOR %%f in (%intdir%\\*.o.stub) DO SET stub_obj_files=!stub_obj_files! .\%%f
+SET STUBOBJS=
+FOR %%f in (%ODIR%\*.o.stub) DO SET STUBOBJS=!STUBOBJS! .\%%f
 
-clang++ -target x86_64-pc-linux-gnu -shared -fuse-ld=lld -ffreestanding -nostdlib -fno-builtin "-L%OO_PS4_TOOLCHAIN%\lib" %libraries% %stub_obj_files% -o "%outputStub%"
+%CC% %STUBOBJS% -o "%ODIR%\%TARGET%_stub.so" %STUBLFLAGS%
 
 REM Create the prx
-%OO_PS4_TOOLCHAIN%\bin\windows\create-lib.exe -in "%outputElf%" --out "%outputOelf%" --paid 0x3800000000000011
+ECHO Creating OELF/PRX...
+%TOOLCHAIN%\bin\windows\create-lib.exe -in "%ODIR%\%TARGET%.elf" -out "%ODIR%\%TARGET%.oelf"
 
 REM Cleanup
-COPY "%outputPrx%" "%outputPath%\%targetname%.prx"
-DEL "%outputPrx%"
+COPY "%ODIR%\%TARGET%.prx" "%SDIR%\%TARGET%.prx"
+DEL "%ODIR%\%TARGET%.prx"
